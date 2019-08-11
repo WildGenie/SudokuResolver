6cfe87172f84daf7bd35a9c96cbfb2e9e796357a
Bilgehan Zeki ÖZAYTAÇ <zekiozaytac@hotmail.com.tr>
2019-08-11 03:01:04 +0300

    Matris olası çözümler çalışır hale getirildi.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,8 +28,10 @@
     /// </summary>
     private void InitializeComponent()
     {
+      this.components = new System.ComponentModel.Container();
       this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
       this.SuspendLayout();
       // 
       // tableLayoutPanel1
@@ -61,33 +63,40 @@
       this.tableLayoutPanel1.Size = new System.Drawing.Size(700, 473);
       this.tableLayoutPanel1.TabIndex = 0;
       // 
-      // textBox1
+      // button1
       // 
-      this.textBox1.Location = new System.Drawing.Point(237, 539);
-      this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(100, 22);
-      this.textBox1.TabIndex = 1;
-      this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      this.button1.Location = new System.Drawing.Point(541, 521);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.Button1_Click);
+      // 
+      // timer1
+      // 
+      this.timer1.Interval = 1000;
+      this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(738, 581);
-      this.Controls.Add(this.textBox1);
+      this.ClientSize = new System.Drawing.Size(735, 581);
+      this.Controls.Add(this.button1);
       this.Controls.Add(this.tableLayoutPanel1);
       this.Name = "Form1";
       this.Text = "Form1";
       this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
-      this.PerformLayout();
 
     }
 
     #endregion
 
     private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Timer timer1;
   }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
   public partial class Form1 : Form
   {
+    private readonly List<konum> matrisResolve = new List<konum>();
+    private int[,] matris;
+
+    private int[,][] test;
+
     public Form1()
     {
       InitializeComponent();
@@ -19,41 +20,96 @@ namespace WindowsFormsApp1
 
     public void yerlestir()
     {
-      var veriler = "305420810487901506029056374850793041613208957074065280241309065508670192096512408".ToCharArray();
-      int[,] matris = new int[9,9];
+      var veriler = "003020600900305001001806400008102900700000008006708200002609500800203009005010300".ToCharArray();
+      matris = new int[9, 9];
 
-      for (int i = 0; i < veriler.Length; i++)
+      for (var i = 0; i < veriler.Length; i++)
       {
         var x = i / 9;
         var y = i % 9;
         Console.WriteLine($"{x},{y}");
-        if (int.TryParse(veriler[i].ToString(), out int rakam))
+        if (int.TryParse(veriler[i].ToString(), out var rakam))
         {
           matris[x, y] = rakam;
-          var kareSatir = (((x / 3) % 3));
-          var kareSutun = (((y / 3) % 3));
-          var kareSira = kareSatir + (kareSutun * 3);
-          Console.WriteLine($"{x},{y} => {kareSatir},{kareSutun}, {kareSira}");
+          var kareSatir = x / 3 % 3;
+          var kareSutun = y / 3 % 3;
+          var kareSira = kareSatir + kareSutun * 3;
+          Console.WriteLine($"{x},{y} => {kareSatir}, {kareSutun}, {kareSira}");
 
-          TextBox label = new TextBox
+          var label = new TextBox
           {
             Text = rakam == 0 ? string.Empty : rakam.ToString(),
             ReadOnly = rakam != 0,
             AutoSize = false,
-            Size = new Size(200,200),
+            Size = new Size(200, 200),
             Font = new Font(FontFamily.GenericMonospace, 16),
-            BackColor = kareSira % 2 == 0 ? Color.AliceBlue : Color.Aquamarine
+            BackColor = kareSira % 2 == 0 ? Color.LightGreen : Color.DeepSkyBlue
           };
-          
+          matrisResolve.Add(new konum(x, y, rakam == 0, rakam));
           tableLayoutPanel1.Controls.Add(label, x, y);
         }
       }
-      
     }
 
     private void Form1_Load(object sender, EventArgs e)
     {
       yerlestir();
+    }
+
+    private void Button1_Click(object sender, EventArgs e)
+    {
+      timer1.Start();
+    }
+
+    private void Timer1_Tick(object sender, EventArgs e)
+    {
+      var sonucBulundu = false;
+      for (var i = 0; i < matris.Length; i++)
+      {
+        var x = i / 9;
+        var y = i % 9;
+        var kareSatir = x / 3 % 3;
+        var kareSutun = y / 3 % 3;
+        var kareSira = kareSatir + kareSutun * 3;
+        var aktif = matrisResolve.Single(k => k.x == x && k.y == y);
+        if (aktif.deger == 0)
+        {
+          //Enumerable.Range(0, matris.Length).Select(m => new konum(m.))
+          var esler = matrisResolve.Where(k => k.deger != 0 && (k.x == x || k.y == y || k.KareSira == kareSira));
+          foreach (var es in esler) aktif.olasi.Remove(es.deger);
+
+          if (aktif.olasi.Count == 1)
+          {
+            sonucBulundu = true;
+            aktif.deger = aktif.olasi[0];
+            (tableLayoutPanel1.GetControlFromPosition(x, y) as TextBox).Text = aktif.deger.ToString();
+          }
+        }
+      }
+
+      if (sonucBulundu == false)
+      {
+        timer1.Stop();
+        MessageBox.Show(matrisResolve.Any(m => m.deger == 0) ? "Sonuc Bulunamıyor!" : "Çözüldü!");
+      }
+    }
+
+    private class konum
+    {
+      public readonly List<int> olasi;
+      public readonly int x;
+      public readonly int y;
+      public int deger;
+
+      public konum(int x, int y, bool bos, int deger)
+      {
+        this.x = x;
+        this.y = y;
+        this.deger = deger;
+        olasi = bos ? Enumerable.Range(1, 9).ToList() : new List<int>();
+      }
+
+      public int KareSira => x / 3 % 3 + y / 3 % 3 * 3;
     }
   }
 }
